2cd642da9a77086891fe78ca9f379d560ca17fb1
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-05-11 14:44:04 +0200

    Refactor math utils into header

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -4,10 +4,10 @@
 #include <limits>
 #include <string>
 #include <iostream>
-#include <type_traits>
 #include <cmath>
 #include "opcodes.hpp"
 #include "future.hpp"
+#include "math.hpp"
 
 static const u2 MAIN_ACCESS_FLAGS = (static_cast<u2>(FieldInfoAccessFlags::ACC_PUBLIC) |
                                      static_cast<u2>(FieldInfoAccessFlags::ACC_STATIC));
@@ -32,53 +32,6 @@ resolve_class(std::unordered_map<std::string_view, ClassFile *> &class_files, CO
               Thread &thread, Frame &frame);
 
 static bool resolve_field_recursive(ClassFile *clazz, CONSTANT_Fieldref_info *field_info);
-
-template<typename T>
-static inline T add_overflow(T a, T b) {
-    // C++20 requires 2's complement for signed integers
-    return future::bit_cast<T>(
-            future::bit_cast<std::make_unsigned_t<T>>(a) + future::bit_cast<std::make_unsigned_t<T>>(b)
-    );
-}
-
-template<typename T>
-static inline T sub_overflow(T a, T b) {
-    // C++20 requires 2's complement for signed integers
-    return future::bit_cast<T>(
-            future::bit_cast<std::make_unsigned_t<T>>(a) - future::bit_cast<std::make_unsigned_t<T>>(b)
-    );
-}
-
-template<typename T>
-static inline T mul_overflow(T a, T b) {
-    // C++20 requires 2's complement for signed integers
-    auto a_u = static_cast<std::make_unsigned_t<T>>(a);
-    auto b_u = static_cast<std::make_unsigned_t<T>>(b);
-    auto result = static_cast<T>(a_u * b_u);
-    return result;
-}
-
-template<typename T>
-static inline T div_overflow(T dividend, T divisor) {
-    if (dividend == std::numeric_limits<T>::min() && divisor == -1) {
-        return dividend;
-    }
-    // TODO test rounding
-    return dividend / divisor;
-}
-
-template<typename F, typename I>
-static inline I floating_to_integer(F f) {
-    if (std::isnan(f)) {
-        return 0;
-    } else if (f > static_cast<F>(std::numeric_limits<I>::max())) {
-        return std::numeric_limits<I>::max();
-    } else if (f < static_cast<F>(std::numeric_limits<I>::min())) {
-        return std::numeric_limits<I>::min();
-    } else {
-        return static_cast<I>(f);
-    }
-}
 
 int interpret(std::unordered_map<std::string_view, ClassFile *> &class_files, ClassFile *main) {
     auto main_method_iter = std::find_if(main->methods.begin(), main->methods.end(),
