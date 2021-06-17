5edff62b4a71e4d115cbfcc14f29a5ade4905870
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-17 11:52:39 +0200

    Add test for retrieving primitive class objects

--- tests/Clazz.java
+++ tests/Clazz.java
@@ -3,9 +3,12 @@ public class Clazz {
     static void println(boolean i) { System.out.println(i); }
 
 	public static void main(String[] args) {
-       // TODO native code doesn't do class loading yet
-        String s = "a";
-        Class<?> a = Object.class;
-        println(a.getName());
+//         TODO native code doesn't do class loading yet
+        String temp = "a";
+
+        println(Object.class.getName());
+        println(Integer.class.getName());
+        println(int.class.getName());
+        println(int[][].class.getName());
 	}
 }
