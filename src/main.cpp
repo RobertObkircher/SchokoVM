e7136b68d5b394abf79acdc42471505c8fdd8e0e
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-21 11:09:44 +0200

    Pass arguments into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <cassert>
 #include <iostream>
 #include <filesystem>
+#include <locale>
+#include <codecvt>
 
 #include "args.hpp"
 #include "jni.h"
 #include "jvm.h"
+#include "classloading.hpp"
 #include "interpreter.hpp"
 
 int main(int argc, char *argv[]) {
@@ -23,17 +26,17 @@ int main(int argc, char *argv[]) {
     assert(args.options == nullptr);
     JavaVMOption hack[10];
     args.options = hack;
-    std::string bootclasspath {"-Xbootclasspath:../jdk/exploded-modules/java.base"};
+    std::string bootclasspath{"-Xbootclasspath:../jdk/exploded-modules/java.base"};
     hack[0].optionString = bootclasspath.data();
-    std::string classpath ="-Djava.class.path=" + arguments->classpath;
+    std::string classpath = "-Djava.class.path=" + arguments->classpath;
     hack[1].optionString = classpath.data();
-    std::string javahome ="-Xjavahome:" + arguments->java_home;
+    std::string javahome = "-Xjavahome:" + arguments->java_home;
     hack[2].optionString = javahome.data();
     args.nOptions = 3;
 
     JavaVM *pvm = nullptr;
     JNIEnv *penv = nullptr;
-    jint status = JNI_CreateJavaVM(&pvm, (void**) &penv, &args);
+    jint status = JNI_CreateJavaVM(&pvm, (void **) &penv, &args);
     if (status != JNI_OK) {
         return status;
     }
@@ -49,8 +52,29 @@ int main(int argc, char *argv[]) {
         pvm->DestroyJavaVM();
         throw std::runtime_error("Couldn't find main method");
     }
-    // TODO args
-    penv->CallStaticVoidMethod(main_class, main_method_id, Reference{nullptr});
+    auto args_array = penv->NewObjectArray(static_cast<jsize>(arguments->remaining.size()),
+                                           reinterpret_cast<jclass>(BootstrapClassLoader::constants().java_lang_String),
+                                           nullptr);
+    if (penv->ExceptionCheck()) {
+        pvm->DestroyJavaVM();
+        throw std::runtime_error("Couldn't find create args array");
+    }
+    for (size_t i = 0; i < arguments->remaining.size(); i++) {
+        const auto &str = arguments->remaining[i];
+        auto str_utf16 = std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}.from_bytes(str);
+        auto str_obj = penv->NewString(reinterpret_cast<const jchar *>(str_utf16.c_str()),
+                                       static_cast<jsize>(str_utf16.length()));
+        if (penv->ExceptionCheck()) {
+            pvm->DestroyJavaVM();
+            throw std::runtime_error("Couldn't create arg string");
+        }
+        penv->SetObjectArrayElement(args_array, static_cast<jsize>(i), str_obj);
+        if (penv->ExceptionCheck()) {
+            pvm->DestroyJavaVM();
+            throw std::runtime_error("Couldn't set arg string");
+        }
+    }
+    penv->CallStaticVoidMethod(main_class, main_method_id, args_array);
 
     if (jthrowable exception = penv->ExceptionOccurred(); exception != nullptr) {
         // TODO?
