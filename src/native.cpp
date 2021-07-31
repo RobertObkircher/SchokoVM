9f7cffa3479976839dc6d2134b9ab247e658d55e
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-07-31 19:51:45 +0200

    Use sanitizers for our version of the native library

--- src/native.cpp
+++ src/native.cpp
@@ -70,9 +70,9 @@ void *get_native_function_pointer(method_info *method) {
     if (dlsym_handle == nullptr) {
         // TODO we might need different flags
 #ifdef __APPLE__
-        dlsym_handle = dlopen("./libNativeLib.dylib", RTLD_LAZY | RTLD_GLOBAL);
+        dlsym_handle = dlopen("./libNativeLibSanitizers.dylib", RTLD_LAZY | RTLD_GLOBAL);
 #else
-        dlsym_handle = dlopen("./libNativeLib.so", RTLD_LAZY | RTLD_GLOBAL);
+        dlsym_handle = dlopen("./libNativeLibSanitizers.so", RTLD_LAZY | RTLD_GLOBAL);
 #endif
         dlsym_handle = dlopen(nullptr, RTLD_LAZY);
         if (auto message = dlerror(); message != nullptr) {
